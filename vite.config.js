f271e0e61f75476e6df916444e2520c7618efb4d
Ander Orbegozo <9and3r@gmail.com>
2024-06-29 18:35:23 +0200

    Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://9and3r.github.io/espresense-plan-helper", // Link page
+});
