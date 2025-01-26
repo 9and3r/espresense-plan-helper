15c997078fba24a80dca1ff19c14d995b8fdc72f
ciaranj <ciaranj@gmail.com>
2025-01-26 19:28:32 +0000

    Translate the room point positions to the origin (0,0)
    Import was previously confusing if the user didn't set their sweethome3d diagram
    to start at 0,0, it could be rendered entirely 'off screen'
    
    
    However, as this affects all the positions that are generated the new
    floorplan will be positionally different compared to previously imported
    ones.

--- src/components/SweetHomeImport.jsx
+++ src/components/SweetHomeImport.jsx
@@ -23,7 +23,8 @@ const SweetHomeImport = ({ setRooms }) => {
       let result = {};
       let id = 1;
       const rooms = xmlDoc.getElementsByTagName("room");
-
+      let minX = Number.MAX_VALUE,
+      minY= Number.MAX_VALUE;
       for (let i = 0; i < rooms.length; i++) {
         let room = rooms[i];
         let roomObject = {
@@ -35,23 +36,44 @@ const SweetHomeImport = ({ setRooms }) => {
         let offset = null;
         for (let z = 0; z < points.length; z++) {
           let point = points[z];
+          let pointX= parseFloat(point.attributes.x.nodeValue);
+          let pointY= parseFloat(point.attributes.y.nodeValue);
           if (offset === null) {
             offset = {
-              x: parseFloat(point.attributes.x.nodeValue),
-              y: parseFloat(point.attributes.y.nodeValue),
+              x: pointX,
+              y: pointY,
             };
             roomObject.points.push({ x: 0, y: 0 });
             continue;
           }
           let currentPoint = {
-            x: parseFloat(point.attributes.x.nodeValue) - offset.x,
-            y: parseFloat(point.attributes.y.nodeValue) - offset.y,
+            x: pointX - offset.x,
+            y: pointY - offset.y,
           };
+          if( pointX <= minX ) {
+            minX= pointX;
+          }
+          if( pointY < minY ) {
+            minY= pointY;
+          }
           roomObject.points.push(currentPoint);
         }
         roomObject.position = offset;
         result[id] = roomObject;
         id++;
+      }
+      // Offset the positions to top-left of drawing
+      for (let roomId in result) {
+        if( minX < 0 ) {
+          result[roomId].position.x += minX;
+        } else {
+          result[roomId].position.x -= minX;
+        }
+        if( minY < 0 ) {
+          result[roomId].position.y += minY;
+        } else {
+          result[roomId].position.y -= minY;
+        }
       }
       setRooms(result);
     },
