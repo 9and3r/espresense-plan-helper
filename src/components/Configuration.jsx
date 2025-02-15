3f44437c39a69149e47f884b58276b0b350b5b6e
Ander Orbegozo <9and3r@gmail.com>
2025-02-15 11:28:16 +0100

    Use mui select

--- src/components/Configuration.jsx
+++ src/components/Configuration.jsx
@@ -1,48 +1,87 @@
-import { TextField } from "@mui/material";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
 
-const Configuration = ({ configuration, setConfiguration, levels, setLevels, selectedLevel, setSelectedLevel }) => {
+const Configuration = ({
+  configuration,
+  setConfiguration,
+  levels,
+  setLevels,
+  selectedLevel,
+  setSelectedLevel,
+}) => {
   function updateLevel(updatedLevel) {
-    let newLevels= [];
-    for(let i=0;i<levels.length;i++){
-      if( levels[i].xmlId !== updatedLevel.xmlId ) {
-        newLevels[i]= levels[i];
+    let newLevels = [];
+    for (let i = 0; i < levels.length; i++) {
+      if (levels[i].xmlId !== updatedLevel.xmlId) {
+        newLevels[i] = levels[i];
       } else {
-        newLevels[i]= updatedLevel;
+        newLevels[i] = updatedLevel;
       }
     }
     setLevels(newLevels);
     setSelectedLevel(updatedLevel);
   }
-  function handleFloorIdChange(e) {
-    selectedLevel.id= e.target.value;
+  const handleFloorIdChange = (e) => {
+    selectedLevel.id = e.target.value;
     updateLevel(selectedLevel);
-  }
-  function handleFloorNameChange(e) {
-    selectedLevel.name= e.target.value;
+  };
+  const handleFloorNameChange = (e) => {
+    selectedLevel.name = e.target.value;
     updateLevel(selectedLevel);
-  }
-  function handleElevationChange(e) {
-    selectedLevel.elevation= e.target.value;
+  };
+  const handleElevationChange = (e) => {
+    selectedLevel.elevation = e.target.value;
     updateLevel(selectedLevel);
-  }
-  function handleHeightChange(e) {
-    selectedLevel.height= e.target.value;
+  };
+  const handleHeightChange = (e) => {
+    selectedLevel.height = e.target.value;
     updateLevel(selectedLevel);
-  }
-    return (
+  };
+  return (
     <>
-     <select
-      value={selectedLevel.xmlId}
-      onChange={e => setSelectedLevel(levels.find( l => l.xmlId === e.target.value))}
-     >
-      {levels.map(level =>
-      <option key={level.xmlId} value={level.xmlId}>{level.name}</option>
-      )};
-    </select>
-      <TextField label="Floor ID" value={selectedLevel.id} onChange={handleFloorIdChange}/>
-      <TextField label="Floor name" value={selectedLevel.name} onChange={handleFloorNameChange}/>
-      <TextField label="Elevation (cm)" value={selectedLevel.elevation} onChange={handleElevationChange}/>
-      <TextField label="Height (cm)" value={selectedLevel.height} onChange={handleHeightChange}/>
+      <FormControl>
+        <InputLabel>Floor</InputLabel>
+        <Select
+          value={
+            selectedLevel && selectedLevel.xmlId ? selectedLevel.xmlId : ""
+          }
+          label="Floor"
+          onChange={(e) =>
+            setSelectedLevel(levels.find((l) => l.xmlId === e.target.value))
+          }
+        >
+          {levels?.map((level) => (
+            <MenuItem key={level.xmlId} value={level.xmlId}>
+              {level.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <TextField
+        label="Floor ID"
+        value={selectedLevel.id}
+        onChange={handleFloorIdChange}
+      />
+      <TextField
+        label="Floor name"
+        value={selectedLevel.name}
+        onChange={handleFloorNameChange}
+      />
+      <TextField
+        label="Elevation (cm)"
+        value={selectedLevel.elevation}
+        onChange={handleElevationChange}
+      />
+      <TextField
+        label="Height (cm)"
+        value={selectedLevel.height}
+        onChange={handleHeightChange}
+      />
     </>
   );
 };
